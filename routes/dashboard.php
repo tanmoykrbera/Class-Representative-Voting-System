0d9aee61049ab771f9daeb3c263b3122f68073ec
tanmoykrbera <tanmoysunny3@gmail.com>
2022-03-28 05:30:00 +0530

    voting commit

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -31,6 +31,55 @@
             </center>
             <hr>
 
-            
+            <div id="mainSection">
+                <div id="profileSection">
+                    <center><img src="../uploads/<?php echo $data['photo']?>" height="100" width="100"></center><br>
+                    <b>Name : </b><?php echo $data['name'] ?><br><br>
+                    <b>Mobile : </b><?php echo $data['mobile'] ?><br><br>
+                    <b>Address : </b><?php echo $data['address'] ?><br><br>
+                    <b>Status : </b><?php echo $status ?>
+                </div>
+                <div id="candidateSection">
+                    <?php
+
+                    if(isset($_SESSION['candidates'])){
+                        $candidates = $_SESSION['candidates'];
+                        for($i=0; $i<count($candidates); $i++){
+                            ?>
+                                <div style="border-bottom: 1px solid #bdc3c7; margin-bottom: 10px">
+                                <img style="float: right" src="../uploads/<?php echo $candidates[$i]['photo']?>" height="80" width="80">
+                                <b>candidate Name : </b><?php echo $candidates[$i]['name']?><br><br>
+                                <b>Votes :</b> <?php echo $candidates[$i]['votes']?><br><br>
+                                <form method="POST" action="../api/vote.php">
+                                <input type="hidden" name="cvotes" value="<?php echo $candidates[$i]['votes'] ?>">
+                                <input type="hidden" name = "cid" value="<?php echo $candidates[$i]['id'] ?>">
+                                <?php
+
+                                if($_SESSION['status']==1){
+                                    ?>
+                                    <button disabled style="padding: 5px; font-size: 15px; background-color: #27ae60; color: white; border-radius: 5px;" type="button">Voted</button>
+                                    <?php
+                                }
+                                else{
+                                    ?>
+                                    <button style="padding: 5px; font-size: 15px; background-color: #3498db; color: white; border-radius: 5px;" type="submit">Vote</button>
+                                    <?php
+                                }
+                                ?>
+                                </form>
+                                </div>
+                            <?php
+                        }
+                    }
+                    else{
+                        ?>
+                            <div style="border-bottom: 1px solid #bdc3c7; margin-bottom: 10px">
+                                <b>No candidates available right now.</b>    
+                            </div>
+                        <?php
+                    }  
+                    ?>
+                </div>
+            </div> 
     </body>
 </html>
