5f496bcf59b40c73a20cae063562181eb5bec730
tanmoykrbera <tanmoysunny3@gmail.com>
2022-04-07 05:30:00 +0530

    voting commit

--- api/vote.php
+++ api/vote.php
@@ -10,6 +10,21 @@
     $update_votes = mysqli_query($connect, "update user set votes='$total_votes' where id='$cid'");
     $update_status = mysqli_query($connect, "update user set status=1 where id='$uid'");
 
-    
+    if($update_status and $update_votes){
+        $getCandidates = mysqli_query($connect, "select name, photo, votes, id from user where role=2 ");
+        $candidates = mysqli_fetch_all($getCandidates, MYSQLI_ASSOC);
+        $_SESSION['candidates'] = $candidates;
+        $_SESSION['status'] = 1;
+        echo '<script>
+                    alert("Voting successfull!");
+                    window.location = "../routes/dashboard.php";
+                </script>';
+    }
+    else{
+        echo '<script>
+                    alert("Voting failed!.. Try again.");
+                    window.location = "../routes/dashboard.php";
+                </script>';
+    }
     
 ?>
